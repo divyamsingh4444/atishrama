1276a7d229cfe1677d03d92299a63b96e3a152d8
punyamsingh <89277920+punyamsingh@users.noreply.github.com>
2024-11-03 05:40:39 +0530

    Refactor authentication setup and improve session handling

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,15 +1,16 @@
-import NextAuth from "next-auth/next";
+import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-export default NextAuth(
-    {
-        providers: [
-            GoogleProvider(
-                {
-                    clientId: process.env.GOOGLE_CLIENT_ID,
-                    clientSecret: process.env.GOOGLE_CLIENT_SECRET
-                }
-            ),
-        ],  
-    }
 
-);
+// Define authOptions separately
+export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+    ],
+    // Any additional NextAuth options go here
+};
+
+// Export NextAuth with the authOptions
+export default NextAuth(authOptions);
